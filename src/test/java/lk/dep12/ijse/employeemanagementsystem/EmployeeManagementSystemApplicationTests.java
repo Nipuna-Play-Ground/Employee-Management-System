367f09b9a43662aa3921bdc6143ac1e3edce0d67
Nipuna-Heshan <nipunaheshan14@gmail.com>
2024-10-18 15:41:19 +0530

    Initial Commit

---
+++ src/test/java/lk/dep12/ijse/employeemanagementsystem/EmployeeManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.dep12.ijse.employeemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
